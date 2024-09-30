6f3730defbb5cb576883f9e91ecd6e676fa7dc52
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-30 23:19:56 +0600

    added the server

--- test2.c
+++ test2.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a,b,c;
-    scanf("%d %d %d",&a ,&b,&c);
-    printf("%d %d %d",a,b,c);
-
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d %d %d", a, b, c);
+    return 0;
 }
