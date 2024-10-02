4d2260a0696e977867187e3aab66259ff294b750
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-03 00:34:46 +0600

    add the loop

---
+++ nestedLoop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            /* code */
+            printf("mock\n", i);
+        }
+        /* code */
+        printf("\n");
+    }
+    return 0;
+}
