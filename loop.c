fd67ce97540237a8d2b87b775fdf842fda809926
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-02 23:18:05 +0600

    add the loop

--- loop.c
+++ loop.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    for(int i=0;i<5;i++){
+    for(int i=1;i<5;i++){
 
     printf("before \n");
     printf("before inside the box %d",i);
+    printf("%d\n",i);
     }
     return 0;
 }
