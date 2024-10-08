c2c3863659fb4d422724e16ee0313d0507326253
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-08 09:01:39 +0600

    add minimum and maximum

--- minOrMax.c
+++ minOrMax.c
@@ -1,15 +1,43 @@
-#include<stdio.h>
-int main(){
-    int a,b,c;
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
 
-    scanf("%d %d %d",&a, &b, &c);
-    if(a>b && b>c){
-        printf("%d",&a);
+    scanf("%d %d %d", &a, &b, &c);
+    if(a>=b && a>=c){
+        printf("%d",a);
     }
-    else if(b>a && b>c){
-        printf("%d",&a);
+    else if(b>=a && b>=c){
+        printf("%d",b);
     }
-    else{
-        printf("%d",&c);
+    else {
+        printf("%d",c);
     }
+
+    // if (a <= b)
+    // {
+    //     // a,c
+    //     if (a <= c)
+    //     {
+    //         printf("%d", a);
+    //     }
+    //     else
+    //     {
+    //         printf("%d", c);
+    //     }
+    // }
+    // else
+    // {
+    //     // b,c
+    //     if (b <= c)
+    //     {
+    //         printf("%d", b);
+    //     }
+    //     else
+    //     {
+    //         printf("%d", c); // c
+
+    //     }
+    // }
+    return 0;
 }
