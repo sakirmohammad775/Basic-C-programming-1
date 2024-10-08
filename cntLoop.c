adca7a47e803e83198411e60867cca2d7be5580f
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-08 10:40:47 +0600

    add odd and even number

---
+++ cntLoop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int cnt = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                cnt ++;
+            }
+        }
+    }
+    printf("%d", cnt);
+    return 0;
+}
