f6c305bbfc9f9a3b2971a50f44f7e496b00ad694
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-14 23:16:03 +0600

    add file

--- help.c
+++ help.c
@@ -5,5 +5,11 @@ int main(){
     if(a>b && a>c){
         printf("%d is the largest number",a);
     }
-    
+    else if(b>c && c>a){
+        printf("%d is the largest number",b);
+    }
+    else{
+        printf("%d is the largest number",c);
+    }
+    return 0;
 }
