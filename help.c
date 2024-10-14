d7efed9ac66672f308c77ace89f9fe9779514261
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-14 23:15:16 +0600

    add file

--- help.c
+++ help.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 int main(){
     int a,b,c;
-    scanf("%d %d %d",&a,&b,c);
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b && a>c){
+        printf("%d is the largest number",a);
+    }
+    else if(b>c && c>a){
+        printf("%d is the largest number",b);
+    }
+    else{
+        printf("%d is the largest number",c);
+    }
+    return 0;
 }
